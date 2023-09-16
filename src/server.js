b9e9d172bc99e8423ff4c2f7789813b1a9f77797
alilgae <ert2423@rit.edu>
2023-09-16 12:12:27 -0400

    refactoring and adding other video/audio files

--- src/server.js
+++ src/server.js
@@ -5,20 +5,37 @@ const mediaHandler = require('./mediaResponses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const onRequest = (request, response) => {
-  console.log(request.url);
+    console.log(request.url);
 
-  switch (request.url) {
-    case '/party.mp4':
-      mediaHandler.getParty(request, response);
-      break;
+    switch (request.url) {
+        case '/party.mp4':
+            mediaHandler.getParty(request, response, 'party.mp4', 'video/mp4');
+            break;
 
-    case '/':
-    default:
-      htmlHandler.getIndex(request, response);
-      break;
-  }
+        case '/bling.mp3':
+            console.log('bling');
+            mediaHandler.getParty(request, response, 'bling.mp3', 'audio/mpeg');
+            break;
+
+        case '/bird.mp4':
+            console.log('bird');
+            mediaHandler.getParty(request, response, 'bird.mp4', 'video/mp4');
+            break;
+
+        case '/page2':
+            htmlHandler.getPage2(request, response);
+            break;
+
+        case '/page3':
+            htmlHandler.getPage3(request, response);
+            break;
+        case '/':
+        default:
+            htmlHandler.getIndex(request, response);
+            break;
+    }
 };
 
 http.createServer(onRequest).listen(port, () => {
-  console.log(`Listening on 127.0.0.1:${port}`);
+    console.log(`Listening on 127.0.0.1:${port}`);
 });
